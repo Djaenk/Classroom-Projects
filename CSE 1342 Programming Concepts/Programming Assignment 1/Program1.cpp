ea5c89d841b26fae6f5c57c39363b427ab09c3f3
Djaenk <Djaenk@users.noreply.github.com>
2019-02-10 22:43:13 -0600

    Completed Assignment

--- CSE 1342 Programming Concepts/Programming Assignment 1/Program1.cpp
+++ CSE 1342 Programming Concepts/Programming Assignment 1/Program1.cpp
@@ -2,113 +2,67 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
-#include <sstream>
+#include <cmath>
 
 long long int stringToDecimal(std::string string) {
 	reverse(string.begin(), string.end());
 	long long int dec = 0;
 	for (int i = 0; i < string.length(); i++) {
-		long long int digit = string[i] - 48;
-		for (int j = 0; j < i; j++) {
-			digit *= 10;
+		if (string[i] == '-') {
+			dec *= -1;
+			continue;
 		}
-		dec += digit;
+		long long int digit = string[i] - 48;
+		dec += digit * pow(10, i);
 	}
 	return dec;
 }
 
 long long int hexToDecimal(std::string hex) {
 	reverse(hex.begin(), hex.end());
+	std::string hex_table = "0123456789ABCDEF";
 	long long int dec = 0;
 	for (int i = 0; i < hex.length(); i++) {
-		long long int digit = 0;
-		if (isalpha(hex[i])) {
-			switch (hex[i]) {
-				case 'A':
-					digit = 10;
-					break;
-				case 'B':
-					digit = 11;
-					break;
-				case 'C':
-					digit = 12;
-					break;
-				case 'D':
-					digit = 13;
-					break;
-				case 'E':
-					digit = 14;
-					break;
-				case 'F':
-					digit = 15;
-					break;
-			}
+		if (hex[i] == '-') {
+			dec *= -1;
+			continue;
 		}
-		else{
-			digit = hex[i] - 48;
-		}
-		for (int j = 0; j < i; j++) {
-			digit *= 16;
-		}
-		dec += digit;
+		int digit = hex_table.std::string::find(hex[i]); //stores position of first occurence of digit in hex table
+		dec += digit * pow(16, i);
 	}
 	return dec;
 }
 
 long long int binaryToDecimal(std::string binary) {
 	reverse(binary.begin(), binary.end());
+	std::string binary_table = "01";
 	long long int dec = 0;
 	for (int i = 0; i < binary.length(); i++) {
-		long long int digit;
-		if (binary[i] == '1') {
-			digit = 1;
+		if (binary[i] == '-') {
+			dec *= -1;
+			continue;
 		}
-		if (binary[i] == '0') {
-			digit = 0;	
-		}
-		for (int j = 0; j < i; j++) {
-			digit *= 2;
-		}
-		dec += digit;
+		int digit = binary_table.std::string::find(binary[i]); //stores position of first occurence of digit in binary table
+		dec += digit * pow(2, i);
 	}
 	return dec;
 }
 
 std::string decimalToHex(long long int dec) {
 	std::string hex = "";
-	for (int i = 0;; i++) {
-		std::string hex_digit = "";
-		long long int digit = dec % 16;
+	std::string hex_table = "0123456789ABCDEF";
+	bool negative = false;
+	if (dec < 0) {
+		dec *= -1;
+		negative = true;
+	}
+	do {
+		int digit = dec % 16;
 		dec /= 16;
-		if (digit > 9) {
-			switch (digit) {
-				case 10:
-					hex_digit = "A";
-					break;
-				case 11:
-					hex_digit = "B";
-					break;
-				case 12:
-					hex_digit = "C";
-					break;
-				case 13:
-					hex_digit = "D";
-					break;
-				case 14:
-					hex_digit = "E";
-					break;
-				case 15:
-					hex_digit = "F";
-					break;
-			}
-		}
-		else{
-			hex_digit = std::to_string(digit);
-		}
-		hex += hex_digit;
-		if (dec == 0) {
-			break;
-		}
+		hex += hex_table[digit];
+	} while (dec != 0);
+	if (negative) {
+		hex += '-';
 	}
 	reverse(hex.begin(), hex.end());
 	return hex;
@@ -116,60 +70,70 @@ std::string decimalToHex(long long int dec) {
 
 std::string decimalToBinary(long long int dec) {
 	std::string binary = "";
-	for (int i = 0;; i++){
-		long long int digit = dec % 2;
-		dec /= 2;
-		binary += std::to_string(digit);
-		if (dec == 0) {
-			break;
-		}
+	std::string binary_table = "01";
+	bool negative = false;
+	if (dec < 0) {
+		dec *= -1;
+		negative = true;
 	}
+	do {
+		int digit = dec % 2;
+		dec /= 2;
+		binary += binary_table[digit];
+	} while (dec != 0);
+	if (negative) {
+		binary += '-';
+	}
+	reverse(binary.begin(), binary.end());
+	return binary;
 }
 
 int main() {
 	std::ifstream input_file;
-	std::string next_line;
 	std::ofstream output_file;
 	output_file.open("output.txt");
 	std::cout << "Enter the name of the input file to be processed, enter nothing for default: ";
 	std::string file_name;
 	std::getline(std::cin, file_name);
-	if (file_name.std::string::empty()) {
-		input_file.std::ifstream::open("input.txt");	
-		if (!input_file.is_open()) {
-			std::cout << "Failed to open input file";
-			return -1;
-		}	
+
+	//check if file opened
+	if (file_name.empty()) {
+		input_file.open("input.txt");	
 	}
 	else {
-		input_file.std::ifstream::open(file_name);
-		if (!input_file.is_open()) {
-			std::cout << "Failed to open input file";
-			return -1;
-		}
+		input_file.open(file_name);
+	}
+	if (!input_file.is_open()) {
+		std::cout << "Failed to open input file";
+		return -1;
 	}
 
-	while (std::getline(input_file, next_line)) {
-		char base;
+	//perform arithmetic operations while file still has more lines
+	while (input_file.peek() != EOF) {
+		std::string base;
 		std::string num1;
-		char operation;
+		std::string operation;
 		std::string num2;
 		long long int operand1;
 		long long int operand2;
 		long long int result;
-		std::istringstream input(next_line);
+		int counter;
 		
-		if (isdigit(next_line[0])) {
+		//I pretty much ignore the purpose of the first line in the input with this
+		if (isdigit(input_file.peek())) {
+			input_file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "Number of lines processed:" << std::endl;
 			continue;
 		}
-		input >> base >> num1 >> operation >> num2;
-		std::cout << std::endl << base << " " << num1 << " " << operation << " " << num2 << std::endl;
-		switch (base) {
+		else {
+			input_file >> base >> num1 >> operation >> num2;
+		}
+
+		switch (base[0]) {
 			case 'B':
 				operand1 = binaryToDecimal(num1);
 				operand2 = binaryToDecimal(num2);
-				std::cout << result << std::endl;
-				switch (operation) {
+				switch (operation[0]) {
 					case '+':
 						result = operand1 + operand2;
 						output_file << decimalToBinary(result) << std::endl;
@@ -181,12 +145,11 @@ int main() {
 					default:
 						output_file << "Invalid operation indicator." << std::endl;
 				}
-				std::cout << operand1 << " " << operation << " " << operand2 << std::endl;
 				break;
 			case 'D':
 				operand1 = stringToDecimal(num1);
 				operand2 = stringToDecimal(num2);
-				switch (operation) {
+				switch (operation[0]) {
 					case '+':
 						result = operand1 + operand2;
 						output_file << operand1 + operand2 << std::endl;
@@ -198,12 +161,11 @@ int main() {
 					default:
 						output_file << "Invalid operation indicator." << std::endl;
 				}
-				std::cout << operand1 << " " << operation << " " << operand2 << std::endl;
 				break;
 			case 'H':
 				operand1 = hexToDecimal(num1);
 				operand2 = hexToDecimal(num2);
-				switch (operation) {
+				switch (operation[0]) {
 					case '+':
 						result = operand1 + operand2;
 						output_file << decimalToHex(result) << std::endl;
@@ -215,11 +177,12 @@ int main() {
 					default:
 						output_file << "Invalid operation indicator." << std::endl;
 				}
-				std::cout << operand1 << " " << operation << " " << operand2 << std::endl;
 				break;
 			default:
 				output_file << "Invalid numeral base system prefix." << std::endl;
 		}
+	counter++;
+	std::cout << "\r" << counter;
 	}
 	input_file.close();
 	output_file.close();
